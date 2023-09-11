dc7781cc0c068f72b06711c719f1f9d6110ce7a8
jadonus <jadongearhart@gmail.com>
2023-09-11 14:08:31 -0400

    mayneeee

---
+++ api/email.py
@@ -0,0 +1,19 @@
+from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+# Initialize Django application
+application = get_wsgi_application()
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # Trigger your Django management command here
+        call_command('emailcheck')
+
+def run_server():
+    server_address = ('', 8000)
+    httpd = HTTPServer(server_address, RequestHandler)
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    run_server()
