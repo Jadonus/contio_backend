df342429969b4ed0d1d3f3d04b6460cbecc0c644
jadonus <jadongearhart@gmail.com>
2023-09-11 09:46:50 -0400

    PLS

--- contio_backend/settings.py
+++ contio_backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -82,11 +82,16 @@ WSGI_APPLICATION = 'contio_backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+postgres://default:l9ckvZ6ahSwQ@ep-morning-glade-94539852-pooler.us-east-1.postgres.vercel-storage.com/verceldb
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'URL': os.environ['POSTGRES_URL'],
+        'NAME': 'contio-backend-postgres',
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST': os.environ['POSTGRES_HOST'],
+        'PORT': 'l9ckvZ6ahSwQ',
     }
 }
 
