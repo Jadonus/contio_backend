58c32c49ee4c7e3f6ea13912c0539184f817f8f3
jadonus <jadongearhart@gmail.com>
2023-09-26 09:06:54 -0400

    please workkkk

--- contio_backend/settings.py
+++ contio_backend/settings.py
@@ -130,7 +130,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'jadongearhart@gmail.com'
-EMAIL_HOST_PASSWORD = 'ftljgejbektelznr'
+EMAIL_HOST_PASSWORD = 'baah ezuj ancm jdfx'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
