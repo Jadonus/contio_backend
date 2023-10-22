56d50135f682622f11a3bbcb36bf7c35f5b4c5e6
jadonus <jadongearhart@gmail.com>
2023-10-22 12:59:37 -0400

    maybee

--- api/email.py
+++ api/email.py
@@ -112,8 +112,9 @@ class handler(BaseHTTPRequestHandler):
             email_status = OriginEmailStatus.objects.get(generatedLink=link)
 
             creation_time = email_status.created_at
-            send_delay = OriginEmailStatus.objects.filter(generatedLink=link).send_delay
-            print('send delay:', send_delay)
+ 
+            send_delay = email_status.send_delay
+            print('delay:', send_delay)
             target_time = creation_time + datetime.timedelta(days=send_delay)
             print('Target Time: ' + target_time.strftime("%m/%d/%Y, %H:%M:%S"))
             tolerance = datetime.timedelta(seconds=60)
