a3786f9e94bd830cb86e60cc9f84ccf17ed32737
jadonus <jadongearhart@gmail.com>
2023-10-24 09:30:43 -0400

    security is good.

--- contio_backend/settings.py
+++ contio_backend/settings.py
@@ -83,16 +83,15 @@ WSGI_APPLICATION = 'contio_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 DATABASES = {
-  'default': {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': 'neondb',
-    'USER': 'jadongearhart',
-    'PASSWORD': 'X9emKn6ZbTYs',
-    'HOST': 'ep-proud-math-69017972.us-east-2.aws.neon.tech',
-    'PORT': '5432',
-  }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neondb',
+        'USER': 'jadongearhart',
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv('URl'),
+        'PORT': '5432',
+    }
 }
-
 
 
 # Password validation
