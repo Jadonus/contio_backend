25a70eda92c57007d8d4223eea0cc685142a39f7
jadonus <jadongearhart@gmail.com>
2023-10-24 09:17:46 -0400

    whooppssss

--- api/email.py
+++ api/email.py
@@ -1,26 +1,28 @@
 # Import necessary modules
+from contio_backend import settings
+from django.core.mail import send_mail
+import json
+import time
+from django.utils import timezone
+from src.models import OriginEmailStatus, Meeting
+from django.conf import settings
+import datetime
+import sys
 from http.server import BaseHTTPRequestHandler
 import os
 import django
 
 django.setup()
-import sys
-import datetime
 
-from django.conf import settings
-from src.models import OriginEmailStatus, Meeting
-from django.utils import timezone
-import time
-import json
-from django.core.mail import send_mail
 
 # Initialize Django
-from contio_backend import settings
 
 # Add your project's base directory to the Python path
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 print('Checking emails...')
 # Your send_scheduled_emails function
+
+
 def send_scheduled_emails():
     print('In Scheduled Email')
     current_time = timezone.now()
@@ -37,8 +39,8 @@ def send_scheduled_emails():
         # Fetch all Meetings with a matching origikn
         print(f"Meeting Generated Link: {email_status.generatedLink}")
 
-
-        matching_meetings = Meeting.objects.filter(origin=email_status.generatedLink)
+        matching_meetings = Meeting.objects.filter(
+            origin=email_status.generatedLink)
         print(matching_meetings)
 
         subject = 'Your scheduling responses'
@@ -59,7 +61,8 @@ def send_scheduled_emails():
                     meeting_info[meeting_date]['count'] += 1
                     meeting_info[meeting_date]['users'].append(user_name)
                 else:
-                    meeting_info[meeting_date] = {'count': 1, 'users': [user_name]}
+                    meeting_info[meeting_date] = {
+                        'count': 1, 'users': [user_name]}
 
             for meeting_date, info in meeting_info.items():
                 count = info['count']
@@ -100,35 +103,38 @@ def send_scheduled_emails():
             print(f"Email sending error: {str(e)}")
 
 # Define the handler for Vercel
+
+
 class handler(BaseHTTPRequestHandler):
     print('ENTERING HANDLER PLEASE LOG')
+
     def do_GET(self):
         # Call your script here when a GET request is made
         unsent_emails = OriginEmailStatus.objects.filter(email_sent=False)
         for email_status in unsent_emails:
-            print('in loop...') 
+            print('in loop...')
             current_time = timezone.now()
             link = email_status.generatedLink
-            email_status = OriginEmailStatus.objects.get(generatedLink=link)
 
             creation_time = email_status.created_at
- 
+
             send_delay = email_status.send_delay
             print('delay:', send_delay)
             target_time = creation_time + datetime.timedelta(days=send_delay)
             print('Target Time: ' + target_time.strftime("%m/%d/%Y, %H:%M:%S"))
             tolerance = datetime.timedelta(seconds=60)
             if current_time >= target_time:
-            # Run the code
+                # Run the code
                 print('sending please please please work. ')
                 send_scheduled_emails()
             else:
                 print('No emails or not time yet.')
-        
+
         # Respond with a success message
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write('Email Checking executed successfully possible please work why'.encode('utf-8'))
+        self.wfile.write(
+            'Email Checking executed successfully possible please work why'.encode('utf-8'))
         return
 # Entry point for Vercel
