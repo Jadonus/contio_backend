46662004e24dd2a0339d07d8b0ec20edaa19091d
jadonus <jadongearhart@gmail.com>
2023-10-25 10:56:24 -0400

    HA

--- api/email.py
+++ api/email.py
@@ -13,7 +13,7 @@ import datetime
 import sys
 from http.server import BaseHTTPRequestHandler
 import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 class handler(BaseHTTPRequestHandler):
     print('ENTERING HANDLER PLEASE LOG')
 
